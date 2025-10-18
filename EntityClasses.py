643c8b98b767211cb439e937a504d8611cf67291
notJachu <jasiustasiu50@interia.pl>
2025-10-18 18:33:28 +0200

    created generation of random multi person bike

--- EntityClasses.py
+++ EntityClasses.py
@@ -1,4 +1,5 @@
-
+import uuid
+import random
 
 class Bike:
     def __init__(self, bike_id, seat_num, is_functional, hourly_rate):
@@ -7,6 +8,12 @@ class Bike:
         self.is_functional = is_functional
         self.hourly_rate = hourly_rate
 
+    @classmethod
+    def random_bike(cls, seat_num = 2):
+        bike_id = str(uuid.uuid4())
+        is_functional = random.choice([True, True, True, False])
+        hourly_rate = round(random.uniform(5.0, 20.0), 2)
+        return cls(bike_id, seat_num, is_functional, hourly_rate)
 
 class MultiPersonBike(Bike):
     def __init__(self, bike_id, seat_config, is_functional, hourly_rate, has_stash, has_roof, rowing_seats_num):
@@ -16,3 +23,21 @@ class MultiPersonBike(Bike):
         self.has_roof = has_roof
         self.rowing_seats_num = rowing_seats_num
 
+    @classmethod
+    def random_multi_person_bike(cls):
+        bike_id = str(uuid.uuid4())
+        seats_num = random.randint(3, 6)
+        seat_config = ''
+        rowing_seats_num = seats_num - 1
+        # TODO: implement seat configuration generation
+        # for i in range(seats_num):
+        #     if random.choice([True, False]):
+        #         seat_config += 'o'
+        #     else:
+        #         seat_config += '+'
+        #         rowing_seats_num += 1
+        is_functional = random.choice([True, True, True, False])
+        hourly_rate = round(random.uniform(10.0, 30.0), 2)
+        has_stash = random.choice([True, False])
+        has_roof = random.choice([True, False])
+        return cls(bike_id, seat_config, is_functional, hourly_rate, has_stash, has_roof, rowing_seats_num)
